d89b86ddc0636a4884c84ee2a59f249537f85218
William Porto <william.porto@nave.rs>
2021-05-31 10:01:56 -0300

    ✨ (Tour): adding some methods

--- Sources/Tour/Tour.swift
+++ Sources/Tour/Tour.swift
@@ -1,11 +1,32 @@
 struct Tour {
-    let constant = "Hello, World!"
-    var variable: String
+    private let constant = "Hello, World!"
+    private var variable: String
+    
+    private var dictionary: [String: Int] = [
+        "total": 12
+    ]
 
     init(v value: String) {
         self.variable = value
 
         print("\(constant) is a constante")
         print("\(variable) is a variable")
+        
+        print("total: \(Int(dictionary["total"] ?? 0))")
+    }
+    
+    func calculateStats(for values: [Int]) throws -> (min: Int?, max: Int?) {
+        if values.isEmpty {
+            fatalError("Cannot calcule statistics for empty arrays")
+        }
+        
+        return (min: values.min(), max: values.max())
+    }
+    
+    func produceMultiplier(for factor: Int) -> ((_ number: Int) -> Int) {
+        func multiplier(_ number: Int) -> Int {
+            return number * factor
+        }
+        return multiplier
     }
 }
--- Sources/Tour/main.swift
+++ Sources/Tour/main.swift
@@ -8,3 +8,17 @@
 import Foundation
 
 let tour = Tour(v: "Hello Swift")
+
+let values = [1, 2, 3, 4, 5, 6, 7, 8]
+let emptyValues = [Int]()
+
+do {
+    let stats = try tour.calculateStats(for: values)
+    print("min: \(stats.min!), max: \(stats.max!)")
+} catch {
+    print(error)
+}
+
+let multiplier = tour.produceMultiplier(for: 5)
+let result = multiplier(5)
+print(result)
